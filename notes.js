89418d09aefee6e8afe94f41490e5bb97040eaf4
DH <dh@DHs-MacBook-Pro.local>
2019-03-18 12:23:29 -0400

    Add internal notes on how to deploy build

---
+++ notes.js
@@ -0,0 +1,17 @@
+/*
+
+Here’s how you make a React App work with Electron to build a standalone React desktop app for MacOS or Windows.
+
+https://medium.com/@brockhoff/using-electron-with-react-the-basics-e93f9761f86f
+
+Note, in the command line before you...
+
+        npm run electron-pack
+
+...you need to...
+
+        npm install typescript —save-dev
+
+...if you don’t install typescript it’ll break and give “Error: Unresolved node modules: typescript”
+
+*/
